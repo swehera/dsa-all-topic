a32f411e1f313c579c3f1233a6371f371cc6d365
Lutfor Rahman Hera <87908048+swehera@users.noreply.github.com>
2023-10-04 13:35:32 +0600

    Create garbage-value.cpp
    An array garbage value in C++ is a value that is stored in an array element but is not initialized properly. This can happen when an array element is declared but not assigned a value, or when an array element is assigned a value that is outside of its range.

---
+++ Array/garbage-value.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+/*
+An array garbage value in C++ is a value that is stored in an array element but is not initialized properly. This can happen when an array element is declared but not assigned a value, or when an array element is assigned a value that is outside of its range.
+
+Here are some examples of array garbage values in C++:
+*/  
+
+
+  
+  int x[4]; // x is declared but not initialized
+  cout << x[0] << endl; // This will print a garbage value
+  cout << x[1] << endl; // This will print a garbage value
+  cout << x[2] << endl; // This will print a garbage value
+  cout << x[3] << endl; // This will print a garbage value
+
+
+
+  
+  return 0;
+}
